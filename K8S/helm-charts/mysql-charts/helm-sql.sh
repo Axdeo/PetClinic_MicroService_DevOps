a7fbef278fe9d2d113baf324f8a413bad92d05c2
Ubuntu <ubuntu@ip-172-31-43-149.eu-west-1.compute.internal>
2023-09-13 12:25:50 +0000

    creation values.yaml pour chart bdd

---
+++ K8S/helm-charts/mysql-charts/helm-sql.sh
@@ -0,0 +1,5 @@
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm install vets-db-mysql bitnami/mysql --values=values.yaml 
+helm install visits-db-mysql bitnami/mysql --values=values.yaml 
+helm install customers-db-mysql bitnami/mysql --values=values.yaml 
