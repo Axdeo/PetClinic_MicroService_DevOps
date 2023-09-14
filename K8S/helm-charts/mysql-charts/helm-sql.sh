08fef4c3bac629414f854e777fcdfb8edc8f9716
Axel de Oliveira <axel.deoliveira1@gmail.com>
2023-09-14 15:53:03 +0000

    ajout des 3 bdd => ajout url dans chacun des deploys des microservices & suppr secret url et user configmap

--- K8S/helm-charts/mysql-charts/helm-sql.sh
+++ K8S/helm-charts/mysql-charts/helm-sql.sh
@@ -1,3 +1,5 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install petclinic-mysql bitnami/mysql -n spring-petclinic --values=values.yaml
+helm install vets-db-mysql bitnami/mysql -n spring-petclinic --values=values.yaml
+helm install customers-db-mysql bitnami/mysql -n spring-petclinic --values=values.yaml
+helm install visits-db-mysql bitnami/mysql -n spring-petclinic --values=values.yaml
