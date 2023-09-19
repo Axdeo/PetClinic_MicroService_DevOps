4beaac2bec68d564553db388923f7266489a68ff
axel <axel.deoliveira1@gmail.com>
2023-09-19 22:42:39 +0200

    changement pull K8S depuis ECR aws + script pour creation Secret ImagePullCred

---
+++ K8S/script_ecr_secret.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl create secret docker-registry aws-ecr-secret \
+  --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password) \
+  --namespace=spring-petclinic
