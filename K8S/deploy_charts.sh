12db626356e0e83e7c2d6abb9746e40833be8aaf
axel <axel.deoliveira1@gmail.com>
2023-09-26 22:15:53 +0200

    creation des scripts de deploiement pour CI/CD // suppression chaos

---
+++ K8S/deploy_charts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+kubectl apply -f namespace.yml
+kubectl apply -f petclinic-configmap.yaml
+kubectl apply -f role.yaml
+kubectl apply -f secret.yaml
+
+kubectl create secret docker-registry aws-ecr-secret \
+  --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password) \
+  --namespace=spring-petclinic
+
+
+cd ./helm-charts/mysql-charts/
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm upgrade --install vets-db-mysql bitnami/mysql -n spring-petclinic --values=values.yaml
+helm upgrade --install customers-db-mysql bitnami/mysql -n spring-petclinic --values=values.yaml
+helm upgrade --install visits-db-mysql bitnami/mysql -n spring-petclinic --values=values.yaml
+
+cd ../api-gateway-chart/
+helm upgrade --install api-gateway . --values=values.yaml --set ingress=$CREATE_INGRESS,service=$SERVICE_TYPE
+
+cd ../customers-service-chart/
+helm upgrade --install customers-service . --values=values.yaml
+
+cd ../vets-service-chart/
+helm upgrade --install vets-service . --values=values.yaml
+
+cd ../visits-service-chart/
+helm upgrade --install visits-service . --values=values.yaml
